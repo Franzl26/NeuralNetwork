281699464fe37005a03399212a73891ad073da54
Frank <frank.lucassen@unibw.de>
2023-01-13 16:51:38 +0100

    Backprop funktionierend für Aufgabe4 Blatt 6

--- Blatt7/aufgabe2.py
+++
@@ -1,3 +0,0 @@
-from hopfield import *
-
-print(train_hopfield([[-1, 1, 1, 0, -1]]))
--- hopfield.py
+++
@@ -1,9 +0,0 @@
-def train_hopfield(vektoren):
-    build = []
-    for i in range(len(vektoren[0])):
-        build.append(0)
-    erg = []
-    for i in range(len(vektoren[0])):
-        erg.append(build)
-
-    return erg
